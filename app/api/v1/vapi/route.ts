0c3d0002f69b62e25ecb3ef60037419678b1a876
twosunaz <twosunazofficial@gmail.com>
2026-01-16 22:46:37 -0700

    1.0.0

--- app/api/v1/vapi/route.ts
+++ app/api/v1/vapi/route.ts
@@ -1,8 +1,8 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import { vapi } from "@/util/vapi/client";
 
 export async function POST(req: Request) {
     const body = req.json();
-    console.log("body", body);
-    vapi.start('bf9bd7a4-e51f-4292-9573-8b09af6bd61f');
+    console.log("n8n body payload:", body);
+    
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    typedRoutes: false,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
