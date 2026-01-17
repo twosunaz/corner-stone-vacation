58c74b6b82e871c9343049721b676e7105a8a6ea
twosunaz <twosunazofficial@gmail.com>
2026-01-16 21:55:17 -0700

    1.0.0

---
+++ app/api/v1/vapi/route.ts
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse } from "next/server";
+import { vapi } from "@/util/vapi/client";
+
+export async function POST(req: Request) {
+    const body = req.json();
+    console.log("body", body);
+    vapi.start('bf9bd7a4-e51f-4292-9573-8b09af6bd61f');
+
+}
